c1e117a517ac54e1a32abe863189a46054c512bf
dmitmel <dmytro.meleshko@gmail.com>
2018-02-02 11:34:26 +0200

    add 'Command not found' message

--- lib/cli/command.d.ts
+++ lib/cli/command.d.ts
@@ -1,6 +1,12 @@
 import * as yargs from 'yargs';
 
 export = class Command {
+  name: string;
+  description: string;
+  builder: yargs.CommandBuilder;
+  handler: (args: yargs.Arguments) => void;
+  didRun: boolean;
+
   constructor(options: {
     name: string;
     description: string;
