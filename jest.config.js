fb27e495bc89fc13336b04c35cf397e4a69bc9ad
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-02-14 20:59:43 +0200

    Jest: exclude lib/cli/commands/**/*.js from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  collectCoverageFrom: ['lib/**/*.js'],
   testMatch: [
     '<rootDir>/lib/**/__tests__/**/*.js',
     '<rootDir>/lib/**/?(*.)(spec|test).js'
   ],
+  collectCoverageFrom: ['lib/**/*.js', '!lib/cli/**/!(command)*.js'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node'
 };
